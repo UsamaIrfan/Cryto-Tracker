632b1cd8be7c4cb3aef2746c8617ee2b69ca5cee
UsamaIrfan <usama.irfan9797@gmail.com>
2023-10-28 16:34:02 +0500

    Initial Commit

---
+++ SwiftCrypto/SwiftCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCryptoApp.swift
+//  SwiftCrypto
+//
+//  Created by Map Mac on 28/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
